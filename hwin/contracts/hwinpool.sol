2f95b257f3c501f63fc3f67d1c6e77edf3452ceb
zhichengh <zhichengh@student.unimelb.edu.au>
2021-02-28 23:21:43 +0800

    commit hwin smart contract

---
+++ hwin/contracts/hwinpool.sol
@@ -0,0 +1,178 @@
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./interfaces/IRewardDistributionRecipient.sol";
+
+contract HWINWrapper {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    IERC20 public HWIN;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function stake(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.add(amount);
+        _balances[msg.sender] = _balances[msg.sender].add(amount);
+        HWIN.safeTransferFrom(msg.sender, address(this), amount);
+    }
+
+    function withdraw(uint256 amount) public virtual {
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        HWIN.safeTransfer(msg.sender, amount);
+    }
+}
+
+contract HWINPool is HWINWrapper, IRewardDistributionRecipient {
+    IERC20 public hwinCash;
+    uint256 public DURATION = 40 days;
+
+    uint256 public starttime;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+    mapping(address => uint256) public deposits;
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    constructor(
+        address hwinCash_,
+        address hwin_,
+        uint256 starttime_
+    ) public {
+        hwinCash = IERC20(hwinCash_);
+        HWIN = IERC20(hwin_);
+        starttime = starttime_;
+    }
+
+    modifier checkStart() {
+        require(block.timestamp >= starttime, "HWINPool: not start");
+        _;
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply() == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable()
+                    .sub(lastUpdateTime)
+                    .mul(rewardRate)
+                    .mul(1e18)
+                    .div(totalSupply())
+            );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return
+            balanceOf(account)
+                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+                .div(1e18)
+                .add(rewards[account]);
+    }
+
+    // stake visibility is public as overriding LPTokenWrapper's stake() function
+    function stake(uint256 amount)
+        public
+        override
+        updateReward(msg.sender)
+        checkStart
+    {
+        require(amount > 0, "HWINPool: Cannot stake 0");
+        uint256 newDeposit = deposits[msg.sender].add(amount);
+        deposits[msg.sender] = newDeposit;
+        super.stake(amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount)
+        public
+        override
+        updateReward(msg.sender)
+        checkStart
+    {
+        require(amount > 0, "HWINPool: Cannot withdraw 0");
+        deposits[msg.sender] = deposits[msg.sender].sub(amount);
+        super.withdraw(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() external {
+        withdraw(balanceOf(msg.sender));
+        getReward();
+    }
+
+    function getReward() public updateReward(msg.sender) checkStart {
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+
+            uint256 ownerRewards = reward.mul(5).div(100);
+            uint256 userRewards = reward.sub(ownerRewards);
+
+            hwinCash.safeTransfer(rewardDistribution, ownerRewards);
+            hwinCash.safeTransfer(msg.sender, userRewards);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function notifyRewardAmount(uint256 reward)
+        external
+        override
+        onlyRewardDistribution
+        updateReward(address(0))
+    {
+        if (block.timestamp > starttime) {
+            if (block.timestamp >= periodFinish) {
+                rewardRate = reward.div(DURATION);
+            } else {
+                uint256 remaining = periodFinish.sub(block.timestamp);
+                uint256 leftover = remaining.mul(rewardRate);
+                rewardRate = reward.add(leftover).div(DURATION);
+            }
+            lastUpdateTime = block.timestamp;
+            periodFinish = block.timestamp.add(DURATION);
+            emit RewardAdded(reward);
+        } else {
+            rewardRate = reward.div(DURATION);
+            lastUpdateTime = starttime;
+            periodFinish = starttime.add(DURATION);
+            emit RewardAdded(reward);
+        }
+    }
+}
